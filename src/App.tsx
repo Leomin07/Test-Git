b8448d2982e2f38e9444fb111311733683680c32
Leomin07 <tranminhsvp@gmail.com>
2022-05-13 22:07:33 +0700

    update base

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Nav from "./components/Nav/Nav";
+import Nav from './components/Nav/Nav';
 
-const MyComponent = () => {
+const App = () => {
     return (
         <div>
-            <Nav/>
+            <Nav />
         </div>
     );
 };
 
-export default MyComponent;
+export default App;
