b8c2c5478bd3005b4690db01717501c83839a95d
Leomin07 <tranminhsvp@gmail.com>
2022-05-13 22:05:39 +0700

    init commit

---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './style.css'
+
+const Nav = () => {
+    return (
+        <div className="nav">
+            <nav className="parent-nav">
+                <a href="#" className="link-image"><img className="img-fluid" src="https://chitchat-react.vercel.app/assets/images/logo/landing-logo.png" alt=""/></a>
+                <div className="navbar">
+                    <ul className="navbar-nav">
+                        <li className="nav-link">MESSENGER</li>
+                        <li className="nav-link">BLOG</li>
+                        <li className="nav-link">AUTHENTICATION</li>
+                        <li className="nav-link">BONUS PAGE</li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Nav;
